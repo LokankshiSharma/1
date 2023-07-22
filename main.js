cef31883652b3d4d7987e7b08d97def948548687
LokankshiSharma <81632228+LokankshiSharma@users.noreply.github.com>
2023-07-22 15:19:25 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup(){
+  canvas=createCanvas(500, 500);
+canvas.position(560,150);
+video=createCapture( VIDEO );
+video.size(550, 500);
+PoseNet=ml5.poseNet(video, modelLoaded);
+PoseNet.on("pose", gotResult)
+}
+function draw(){
+  background("cadetblue")
+}
+function modelLoaded(){
+  console.log("PoseNet is initialized");
+}
+function gotResult(results){
+if (results.length>0)
+{
+console.log(results)
+}
+}
